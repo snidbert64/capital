80984e25e93ce1189454dd9aeffe713865012c3c
snidbert64 <zachalotz@gmail.cp>
2019-05-06 23:57:45 -0500

    added ability to post... i think.

--- garbage.js
+++ garbage.js
@@ -5,47 +5,41 @@ $(document).ready(function() {
     authDomain: 'https://capital-1556235479390.firebaseio.com/',
     projectId: 'capital-1556235479390'
   });
-  
-  var provider = new firebase.auth.GoogleAuthProvider();
-
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-
-    console.log(user);
-    // ...
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
-    
-
-    console.log("It's");
-
-
 
     // import("firebase/app");
     // import("firebase/firestore");
     
     var db = firebase.firestore();
 
+  // Add a new document in collection "cities"
+
+  console.log("It's");
+
+  $("#add-chat").on("click", function(){
+    db.collection("posts").doc("LA").set({
+      author: "Nobody",
+      content: $("#chat-input").val(),
+      date: new Date(),
+      score: 0
+    })
+    .then(function() {
+      console.log("Document successfully written!");
+    })
+    .catch(function(error) {
+      console.error("Error writing document: ", error);
+    });
+  });
+
     db.collection("posts").where("score", ">", -1)
     .onSnapshot(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            var post = $("<div>");
-            $("<h4>"+ doc.data().author + "</h4>").appendTo(post);
-            $("<button data-author='"+ doc.data().author +"' data-id='" + doc.data().id + "'>" + doc.data().score + "</button>").appendTo(post);
-            $("<p>" + doc.data().content + "</p>").appendTo(post);
-            post.appendTo($("#chat"));
-        });
+      $("#chat").empty();
+      querySnapshot.forEach(function(doc) {
+          var post = $("<div>");
+          $("<h4>"+ doc.data().author + "</h4>").appendTo(post);
+          $("<button data-author='"+ doc.data().author +"' data-id='" + doc.data().id + "'>" + doc.data().score + "</button>").appendTo(post);
+          $("<p>" + doc.data().content + "</p>").appendTo(post);
+          post.appendTo($("#chat"));
+      });
     });
 
 
